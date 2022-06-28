03afbbaa398fbe6fda300c86d65a72be169204b8
LoqLamp <david04492@googlemail.com>
2022-06-28 12:41:32 +0100

    implimented function for player choice

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+let userChoice = 0;
+
+let userInput = prompt("Rock Paper or Scissors?").toLowerCase();
+console.log(userInput);
+
+// Loop for invalid userInput until a correct value in entered
+
+while (["rock", "paper", "scissors"].indexOf(userInput) === -1) {
+  userInput = prompt("Please enter valid choice").toLowerCase();
+}
+
+assignUserChoice(userInput);
+
+function assignUserChoice(userInput) {
+  if (userInput === "rock") {
+    return (userChoice = 1); // Rock
+  } else if (userInput === "paper") {
+    return (userChoice = 2); // Paper
+  } else if (userInput === "scissors") {
+    return (userChoice = 3); // Scissors
+  }
+}
+console.log(userChoice);
+
+function getComputerChoice() {}
+let result = Math.floor(Math.random() * 3) + 1;
+console.log(result);
