c684a1a6e2dfc33d3121cb52a63b621426bbf72c
LoqLamp <david04492@googlemail.com>
2022-07-04 18:52:17 +0100

    added method to show player and computer choices

--- scripts/script.js
+++ scripts/script.js
@@ -23,13 +23,29 @@ scissors.addEventListener("click", function () {
 
 function assignUserChoice(userInput) {
   if (userInput === "rock") {
-    return (userChoice = "1"), getComputerChoice(); // Rock
+    return (userChoice = "1"), insertPlayerImage(1), getComputerChoice(); // Rock
   } else if (userInput === "paper") {
-    return (userChoice = "2"), getComputerChoice(); // Paper
+    return (userChoice = "2"), insertPlayerImage(2), getComputerChoice(); // Paper
   } else if (userInput === "scissors") {
-    return (userChoice = "3"), getComputerChoice(); // Scissors
+    return (userChoice = "3"), insertPlayerImage(3), getComputerChoice(); // Scissors
   }
 }
+
+function insertPlayerImage(playerChoice) {
+  let playerChoiceImage = document.querySelector(".player-choice > img");
+  const playerC = document.querySelector(".player-choice");
+  if (playerChoice === 3) {
+    playerChoiceImage.src = "../images/noun-scissors.png";
+    playerC.replaceChild(playerChoiceImage, playerChoiceImage);
+  } else if (playerChoice === 2) {
+    playerChoiceImage.src = "../images/noun-hand-477922.png";
+    playerC.replaceChild(playerChoiceImage, playerChoiceImage);
+  } else if (playerChoice === 1) {
+    playerChoiceImage.src = "../images/noun-fist-477918.png";
+    playerC.replaceChild(playerChoiceImage, playerChoiceImage);
+  }
+}
+
 console.log(userChoice);
 // getComputerChoice();
 
@@ -37,13 +53,23 @@ function getComputerChoice() {
   let result = Math.floor(Math.random() * 3) + 1;
   userChoice = userChoice + result.toString();
   console.log(result);
-  getScore();
+  insertComputerImage(result), getScore();
 }
 
-//11, 22, 33 Draw
-//12, 23, 31 Lose
-//13, 21, 32 Win
-// getScore();
+function insertComputerImage(computerChoice) {
+  let computerChoiceImage = document.querySelector(".computer-choice > img");
+  const computerC = document.querySelector(".computer-choice");
+  if (computerChoice === 3) {
+    computerChoiceImage.src = "../images/noun-scissors.png";
+    computerC.replaceChild(computerChoiceImage, computerChoiceImage);
+  } else if (computerChoice === 2) {
+    computerChoiceImage.src = "../images/noun-hand-477922.png";
+    computerC.replaceChild(computerChoiceImage, computerChoiceImage);
+  } else if (computerChoice === 1) {
+    computerChoiceImage.src = "../images/noun-fist-477918.png";
+    computerC.replaceChild(computerChoiceImage, computerChoiceImage);
+  }
+}
 
 function getScore() {
   if (userChoice === "11" || userChoice === "22" || userChoice === "33") {
@@ -61,14 +87,23 @@ function getScore() {
     userScore++;
   }
   console.log(userScore, computerScore);
-  if (userScore >= computerScore + 2) {
-    alert("win");
-  }
 
   let playerScore = document.querySelector(".player-score");
   playerScore.textContent = userScore;
   let compScore = document.querySelector(".computer-score");
   compScore.textContent = computerScore;
+
+  if (userScore === 5) {
+    playerScore.textContent = `You Win: ${userScore}`;
+    compScore.textContent = `You Lose: ${computerScore}`;
+    userScore = 0;
+    computerScore = 0;
+  } else if (computerScore === 5) {
+    compScore.textContent = "Win";
+    playerScore.textContent = "Lose";
+    userScore = 0;
+    computerScore = 0;
+  }
 }
 
 console.log(userScore, computerScore, userChoice);
